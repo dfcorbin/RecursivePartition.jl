c4e405000706270600477975d83c5fc068997745
Documenter.jl <documenter@juliadocs.github.io>
2021-01-08 10:23:48 +0000

    build based on 1728265

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RecursivePartition","category":"page"},{"location":"#RecursivePartition","page":"Home","title":"RecursivePartition","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RecursivePartition]","category":"page"}]
+}
